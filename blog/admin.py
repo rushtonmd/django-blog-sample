e166d21f9f1620960ff6e395d36718ae520e76e6
Mark D Rushton <rushtonmd@github.com>
2018-01-25 11:29:57 -0800

    created models for Posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+
+# Register the Post model to show on the admin site
+# Set the template class to use PostAdmin
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create a custom manager for the Post models
+# to retrieve all posts that have been published
+class PublishedManager(models.Manager):
+    # get_queryset returns the queryset to be executed
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+    # Create the constants for status. We used tuple because these
+    # are immutable properties
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(
+        User,
+        related_name='blog_posts',
+        on_delete=models.PROTECT
+        )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='draft'
+        )
+
+    objects = models.Manager()  # The default manager
+    published = PublishedManager()  # Custom manager for published posts
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
